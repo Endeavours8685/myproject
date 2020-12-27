a3904274d779c177a023f160ca2ffcdc0ce2f124
Endeavours8685 <kassalini29@gmail.com>
2020-12-27 13:42:01 +0530

    Third commit

---
+++ nextstep.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+#include<vector>
+using namesapce std;
+
+int main()
+{
+	vector<int> a;
+	int B[]={12,23,34,45};
+	a.assign(A,A+3);
+	auto it1=a.begin();
+	auto it2=a.end();
+	cout<<*it1<<" "<<*it2<<endl;
+	return 0;
+}
